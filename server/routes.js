2d5c30804e755b0729e112e384e66f4f5a5f32a9
ETBunce <etbunce@gmail.com>
2023-04-10 20:48:49 -0600

    fixed a route

--- server/routes.js
+++ server/routes.js
@@ -6,6 +6,6 @@ const controllers = require("./controllers.js");
 
 router.get('/', (req, res)=> { res.send('Hello , world!'); });
 router.get('/getGrid', controllers.getGrid);
-router.get('playerScores', controllers.getPlayerScores);
+router.get('/playerScores', controllers.getPlayerScores);
 
 module.exports = router;
