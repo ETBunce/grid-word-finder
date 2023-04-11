4a97b30956d09fbf84bfddb68b3eb96ab47ee94b
Briana Corp <47436169+BrianaCorp@users.noreply.github.com>
2023-04-10 20:58:34 -0600

    edited gridgame, index.css, routes

--- client/src/components/GridGame.js
+++ client/src/components/GridGame.js
@@ -3,27 +3,47 @@ import axios from "axios";
 
 export function GridGame() {
     const [board, setBoard] = useState([]);
+    const [grid, setGrid] = useState("");
 
     // get function gets game board, players
     useEffect(() => {
         axios.get("http://localhost:4000/game")
         .then((res) => {
-            setBoard(res.data.letters);
+            setGrid(res.data);
         })
         .catch(err => console.log(err));
     }, []);
+
+    useEffect(() => {
+        setBoard(board => [...board, grid.split("")]);
+    }, [grid]);
 
     // onclick function for buttons do a post method
 
     return (
         <center>
             <h1>Main Game</h1>
-            <button>{board[0]}</button>
-            <button>{board[1]}</button>
-            <button>{board[2]}</button>
-            <button>{board[3]}</button>
-            <button>{board[4]}</button>
-            <button>{board[5]}</button>
+            <div id = "grid">
+                <button>{board[0]}</button>
+                <button>{board[1]}</button>
+                <button>{board[2]}</button>
+                <button>{board[3]}</button>
+                <br />
+                <button>{board[4]}</button>
+                <button>{board[5]}</button>
+                <button>{board[6]}</button>
+                <button>{board[7]}</button>
+                <br />
+                <button>{board[8]}</button>
+                <button>{board[9]}</button>
+                <button>{board[10]}</button>
+                <button>{board[11]}</button>
+                <br />
+                <button>{board[12]}</button>
+                <button>{board[13]}</button>
+                <button>{board[14]}</button>
+                <button>{board[15]}</button>
+            </div>
         </center>
     );
 }
--- server/routes.js
+++ server/routes.js
@@ -4,6 +4,8 @@ const router = express.Router();
 
 const controllers = require("./controllers.js");
 
+router.get("/game", controllers.getGrid);
+
 router.get('/', (req, res)=> { res.send('Hello , world!'); });
 
 module.exports = router;
