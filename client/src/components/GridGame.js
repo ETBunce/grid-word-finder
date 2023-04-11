276de88bc3ebf58f41a339a40a30a04e741042ad
Briana Corp <47436169+BrianaCorp@users.noreply.github.com>
2023-04-10 20:22:37 -0600

    fixed a thing in index, edited gridgame

--- client/src/components/GridGame.js
+++ client/src/components/GridGame.js
@@ -1,9 +1,29 @@
-export function GridGame() {
+import { useState, useEffect } from "react";
+import axios from "axios";
 
+export function GridGame() {
+    const [board, setBoard] = useState([]);
+
+    // get function gets game board, players
+    useEffect(() => {
+        axios.get("http://localhost:4000/game")
+        .then((res) => {
+            setBoard(res.data.letters);
+        })
+        .catch(err => console.log(err));
+    }, []);
+
+    // onclick function for buttons do a post method
 
     return (
         <center>
             <h1>Main Game</h1>
+            <button>{board[0]}</button>
+            <button>{board[1]}</button>
+            <button>{board[2]}</button>
+            <button>{board[3]}</button>
+            <button>{board[4]}</button>
+            <button>{board[5]}</button>
         </center>
     );
 }
