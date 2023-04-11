8e4d53646b573797bf2282f4f18141eda5e2a7cd
ETBunce <etbunce@gmail.com>
2023-04-10 20:43:15 -0600

    Fixed typo in lobby model

--- server/routes.js
+++ server/routes.js
@@ -5,5 +5,6 @@ const router = express.Router();
 const controllers = require("./controllers.js");
 
 router.get('/', (req, res)=> { res.send('Hello , world!'); });
+router.get('/getGrid', controllers.getGrid);
 
 module.exports = router;
